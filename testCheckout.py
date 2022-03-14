570d4b521bb2368e22829d66470d3be839001e94
leigh966 <leigh966@hotmail.com>
2022-03-14 16:02:56 +0000

    Refactored to use test fixture rather that manually instantiating the checkout at the start of each test

--- testCheckout.py
+++ testCheckout.py
@@ -1,9 +1,13 @@
 from Checkout import Checkout
+import pytest
 
-def test_CanAddItemPrice():
-    co = Checkout()
-    co.addItemPrice("a", 1)
+@pytest.fixture()
+def checkout():
+    checkout = Checkout()
+    return checkout
 
-def test_CanAddItem():
-    co = Checkout()
-    co.addItem("a")
+def test_CanAddItemPrice(checkout):
+    checkout.addItemPrice("a", 1)
+
+def test_CanAddItem(checkout):
+    checkout.addItem("a")
