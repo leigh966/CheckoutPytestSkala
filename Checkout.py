e974009075f6e60152379d131dfd7a21b69ac269
leigh966 <leigh966@hotmail.com>
2022-03-15 13:50:16 +0000

    set most recently added test to skip to refactor checkout class for the implementation of the new functionality

--- Checkout.py
+++ Checkout.py
@@ -1,12 +1,17 @@
 class Checkout:
+
     def __init__(self):
         self.prices = {}
-        self.total = 0
+        self.discounts = {}
+        self.items = []
     def addItemPrice(self, item, price):
         self.prices[item] = price
     def addItem(self, item):
-        self.total += self.prices[item]
+        self.items.append(item)
     def calculateTotal(self):
-        return self.total
+        total = 0
+        for item in self.items:
+            total += self.prices[item]
+        return total
     def addDiscount(self, item, num, price):
-        pass
+        self.discounts[item, num] = price
