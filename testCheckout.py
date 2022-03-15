fdea054d29c390c4f181f5fba79f5db80a209378
leigh966 <leigh966@hotmail.com>
2022-03-15 13:25:04 +0000

    Refactored by moving duplicated code to add item price to the test fixture

--- testCheckout.py
+++ testCheckout.py
@@ -4,17 +4,16 @@ import pytest
 @pytest.fixture()
 def checkout():
     checkout = Checkout()
+    checkout.addItemPrice("a", 1)
+    checkout.addItemPrice("b", 2)
     return checkout
 
 
 def test_CanCalculateTotal(checkout):
-    checkout.addItemPrice("a", 1)
     checkout.addItem("a")
     assert checkout.calculateTotal() == 1
 
 def test_CanCalculateCorrectTotal(checkout):
-    checkout.addItemPrice("a", 1)
-    checkout.addItemPrice("b", 2)
     checkout.addItem("a")
     checkout.addItem("b")
     assert checkout.calculateTotal()==3
